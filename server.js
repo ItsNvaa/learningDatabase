be89f15fd4038bbd9a1de7552063275e08af7807
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-05-17 02:11:30 -0700

    main

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "src");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  database: "punyanvaa",
+  user: "root",
+  password: "",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("connected");
+
+  const sql = "SELECT * FROM databasemurid";
+  db.query(sql, (err, result) => {
+    const users = JSON.parse(JSON.stringify(result));
+    console.log("hasil", users);
+    app.get("/", (req, res) => {
+      res.render("index", { users: users });
+    });
+  });
+});
+
+app.listen(8000, () => {
+  console.log("server ready...");
+});
